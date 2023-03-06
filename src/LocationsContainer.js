7023fb6816afa72725e0d61bbda8ea2f33a00d5b
Dakota Vikdal <dakota.james19@hotmail.com>
2023-03-06 16:42:22 -0500

    dropdown

--- src/LocationsContainer.js
+++ src/LocationsContainer.js
@@ -18,6 +18,12 @@ function LocationsContainer() {
    
     return (
         <div>
+            <select id="dropdown">
+                <option value="Locations">Locations</option>
+                <option value="Trinidad">Trinidad</option>
+                <option value="Egypt">Egypt</option>
+                <option value="Alaska">Alaska</option>
+            </select>
             <TripExperience />
             <LocationList locations={locations}/>
         </div>
