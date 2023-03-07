86138134fb4c0e84b731865a8b2bb523826e9440
Dakota Vikdal <dakota.james19@hotmail.com>
2023-03-07 11:33:47 -0500

    Nothing much

--- src/TripExperience.js
+++ src/TripExperience.js
@@ -3,33 +3,33 @@
 
 //Form Post
 function TripExperience() {
-    
-    const newRating = {
-        comments
-       
-      }
-    
-    
-    fetch( `http://localhost:3000/locations`, {
-        method: 'POST', 
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify( newRating )
-      } )
-        .then( r => r.json() )
-        .then( addRatingToState )
-      e.target.reset()
-    
-    
-    return (
-        <div className="new-plant-form">
-        <h2>New Rating</h2>
-        <form onSubmit={ handleSubmit } >
-          <input onChange={ handleName } type="text" name="name" 
-            placeholder="Rate Us!" />
-        </form>
-       </div>
-    )
-
-}
-
-export default TripExperience
+    // 
+    // const newRating = {
+        // comments
+    //    
+    //   }
+    // 
+    // 
+    // fetch( `http://localhost:3000/locations`, {
+        // method: 'POST', 
+        // headers: { 'Content-Type': 'application/json' },
+        // body: JSON.stringify( newRating )
+    //   } )
+        // .then( r => r.json() )
+        // .then( addRatingToState )
+    //   e.target.reset()
+    // 
+    // 
+    // return (
+        // <div className="new-plant-form">
+        {/* <h2>New Rating</h2> */}
+        {/* <form onSubmit={ handleSubmit } > */}
+          {/* <input onChange={ handleName } type="text" name="name"  */}
+            // placeholder="Rate Us!" />
+        {/* </form> */}
+       {/* </div> */}
+    // )
+// 
+ }
+// 
+ export default TripExperience
