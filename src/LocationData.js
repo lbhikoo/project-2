25173bad50dcea6d725b82cce03dee43a438c78a
Dakota Vikdal <dakota.james19@hotmail.com>
2023-03-09 10:18:29 -0500

    db.json split method

--- src/LocationData.js
+++ src/LocationData.js
@@ -3,13 +3,23 @@ import "./LocationData.css"
 
 function LocationData({location, todo, food, hotel, history, image}) {
 
+    function splitterTool(string) {
+        const anArray = string.split('\n')
+        const pElements = anArray.map(p => <p> {p} </p>)
+        return pElements
+    }
+
     return(
         <div>
             <h1>{location}</h1>
-            <p>{todo}</p>
-            <p>{food}</p>
-            <p>{hotel}</p>
-            <p>{history}</p>
+            <h3>Things to do</h3>
+            <div>{splitterTool(todo)}</div>
+            <h3>Food to eat</h3>
+            <div>{splitterTool(food)}</div>
+            <h3>Hotels to stay at</h3>
+            <p>{splitterTool(hotel)}</p>
+            <h3>History</h3>
+            <p>{splitterTool(history)}</p>
             <img src={image}></img>
         </div>
     )
